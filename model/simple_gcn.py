453b1c9e5cbbd2e26dcb9331b9faf389de6b91fb
DBLearner-stack <67966280+DBLearner-stack@users.noreply.github.com>
2020-07-22 06:55:06 +0800

    Add files via upload

---
+++ model/simple_gcn.py
@@ -0,0 +1,85 @@
+
+
+# graph embedding model
+
+class arguments():
+    def __init__(self):
+        self.cuda = False
+        self.fastmode = False
+        self.seed = 42
+        self.epochs = 500
+        self.lr = 0.01
+        self.weight_decay = 5e-4
+        self.hidden = 16
+        self.dropout = 0.5
+        
+args = arguments()
+
+import math
+import torch
+from torch.nn.parameter import Parameter
+from torch.nn.modules.module import Module
+from model import taylor_decompose
+
+class GraphConvolution(Module):
+    """
+    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
+    """
+
+    def __init__(self, in_features, out_features, bias=True):
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        if bias:
+            self.bias = Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, adj):
+        support = torch.mm(input, self.weight)
+        output = torch.spmm(adj, support)
+        if self.bias is not None:
+            return output + self.bias
+        else:
+            return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+               + str(self.in_features) + ' -> ' \
+               + str(self.out_features) + ')'
+
+
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class GCN(nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout):
+        super(GCN, self).__init__()
+        
+        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc2 = GraphConvolution(nhid, nclass)
+        self.decompose = taylor_decompose.DTD()
+        self.fc=nn.Linear(nclass,1)
+        self.dropout = dropout
+
+    def forward(self, x, adj):
+        x = F.relu(self.gc1(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gc2(x, adj)
+        x=self.fc(x)
+        # module_list = taylor_decompose.model_flattening(self.gc2)
+        # act_store_model = taylor_decompose.ActivationStoringNet(module_list)
+        # module_stack, embed = act_store_model(x)
+        # relevance = self.decompose(module_stack, embed, 1000)
+
+        #        return F.log_softmax(relevance, dim=nfeat)
+        return x
---
+++ model/taylor_decompose.py
@@ -0,0 +1,300 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pdb
+from collections import OrderedDict
+# from .resnet import BasicBlock, Bottleneck
+
+
+def model_flattening(module_tree):
+    module_list = []
+    children_list = list(module_tree.children())
+    if len(children_list) == 0 or isinstance(module_tree, BasicBlock) or \
+        isinstance(module_tree, Bottleneck):
+        return [module_tree]
+    else:
+        for i in range(len(children_list)):
+            module = model_flattening(children_list[i])
+            module = [j for j in module]
+            module_list.extend(module)
+        return module_list
+
+
+class ActivationStoringNet(nn.Module):
+    def __init__(self, module_list):
+        super(ActivationStoringNet, self).__init__()
+        self.module_list = module_list
+
+    def basic_block_forward(self, basic_block, activation):
+        identity = activation
+
+        basic_block.conv1.activation = activation
+        activation = basic_block.conv1(activation)
+        activation = basic_block.relu(basic_block.bn1(activation))
+        basic_block.conv2.activation = activation
+        activation = basic_block.conv2(activation)
+        activation = basic_block.bn2(activation)
+        if basic_block.downsample is not None:
+            for i in range(len(basic_block.downsample)):
+                basic_block.downsample[i].activation = identity
+                identity = basic_block.downsample[i](identity)
+            basic_block.identity = identity
+        basic_block.activation = activation
+        output = activation + identity
+        output = basic_block.relu(output)
+
+        return basic_block, output
+
+    def bottleneck_forward(self, bottleneck, activation):
+        identity = activation
+
+        bottleneck.conv1.activation = activation
+        activation = bottleneck.conv1(activation)
+        activation = bottleneck.relu(bottleneck.bn1(activation))
+        bottleneck.conv2.activation = activation
+        activation = bottleneck.conv2(activation)
+        activation = bottleneck.relu(bottleneck.bn2(activation))
+        bottleneck.conv3.activation = activation
+        activation = bottleneck.conv3(activation)
+        activation = bottleneck.bn3(activation)
+        if bottleneck.downsample is not None:
+            for i in range(len(bottleneck.downsample)):
+                bottleneck.downsample[i].activation = identity
+                identity = bottleneck.downsample[i](identity)
+            bottleneck.identity = identity
+        bottleneck.activation = activation
+        output = activation + identity
+        output = bottleneck.relu(output)
+
+        return bottleneck, output
+
+    def forward(self, x):
+        module_stack = []
+        activation = x
+
+        for i in range(len(self.module_list)):
+            module = self.module_list[i]
+            if isinstance(module, BasicBlock):
+                module, activation = self.basic_block_forward(module, activation)
+                module_stack.append(module)
+            elif isinstance(module, Bottleneck):
+                module, activation = self.bottleneck_forward(module, activation)
+                module_stack.append(module)
+            else:
+                module.activation = activation
+                module_stack.append(module)
+                activation = module(activation)
+                if isinstance(module, nn.AdaptiveAvgPool2d):
+                    activation = activation.view(activation.size(0), -1)
+
+        output = activation
+
+        return module_stack, output
+
+
+
+
+class DTD(nn.Module):
+    def __init__(self, lowest=0., highest=1.):
+        super(DTD, self).__init__()
+        self.lowest = lowest
+        self.highest = highest
+
+    def forward(self, module_stack, y, class_num):
+        R = torch.eye(class_num)[torch.max(y, 1)[1]]
+
+        for i in range(len(module_stack)):
+            module = module_stack.pop()
+            if len(module_stack) == 0:
+                if isinstance(module, nn.Linear):
+                    activation = module.activation
+                    R = self.backprop_dense_input(activation, module, R)
+                elif isinstance(module, nn.Conv2d):
+                    activation = module.activation
+                    R = self.backprop_conv_input(activation, module, R)
+                else:
+                    raise RuntimeError(f'{type(module)} layer is invalid initial layer type')
+            elif isinstance(module, BasicBlock):
+                R = self.basic_block_R_calculate(module, R)
+            elif isinstance(module, Bottleneck):
+                R = self.bottleneck_R_calculate(module, R)
+            else:
+                if isinstance(module, nn.AdaptiveAvgPool2d):
+                    R = R.view(R.size(0), R.size(1), 1, 1)
+                activation = module.activation
+                R = self.R_calculate(activation, module, R)
+
+        return R
+
+    def basic_block_R_calculate(self, basic_block, R):
+        if basic_block.downsample is not None:
+            identity = basic_block.identity
+        else:
+            identity = basic_block.conv1.activation
+        activation = basic_block.activation
+        R0, R1 = self.backprop_skip_connect(activation, identity, R)
+        R0 = self.backprop_conv(basic_block.conv2.activation, basic_block.conv2, R0)
+        R0 = self.backprop_conv(basic_block.conv1.activation, basic_block.conv1, R0)
+        if basic_block.downsample is not None:
+            for i in range(len(basic_block.downsample)-1, -1, -1):
+                R1 = self.R_calculate(basic_block.downsample[i].activation,
+                                      basic_block.downsample[i], R1)
+        else:
+            pass
+        R = self.backprop_divide(R0, R1)
+
+        return R
+
+    def bottleneck_R_calculate(self, bottleneck, R):
+        if bottleneck.downsample is not None:
+            identity = bottleneck.identity
+        else:
+            identity = bottleneck.conv1.activation
+        activation = bottleneck.activation
+        R0, R1 = self.backprop_skip_connect(activation, identity, R)
+        R0 = self.backprop_conv(bottleneck.conv3.activation, bottleneck.conv3, R0)
+        R0 = self.backprop_conv(bottleneck.conv2.activation, bottleneck.conv2, R0)
+        R0 = self.backprop_conv(bottleneck.conv1.activation, bottleneck.conv1, R0)
+        if bottleneck.downsample is not None:
+            for i in range(len(bottleneck.downsample)-1, -1, -1):
+                R1 = self.R_calculate(bottleneck.downsample[i].activation,
+                                      bottleneck.downsample[i], R1)
+        else:
+            pass
+        R = self.backprop_divide(R0, R1)
+
+        return R
+
+    def R_calculate(self, activation, module, R):
+        if isinstance(module, nn.Linear):
+            R = self.backprop_dense(activation, module, R)
+            return R
+        elif isinstance(module, nn.Conv2d):
+            R = self.backprop_conv(activation, module, R)
+            return R
+        elif isinstance(module, nn.BatchNorm2d):
+            R = self.backprop_bn(R)
+            return R
+        elif isinstance(module, nn.ReLU):
+            R = self.backprop_relu(activation, R)
+            return R
+        elif isinstance(module, nn.MaxPool2d):
+            R = self.backprop_max_pool(activation, module, R)
+            return R
+        elif isinstance(module, nn.AdaptiveAvgPool2d):
+            R = self.backprop_adap_avg_pool(activation, R)
+            return R
+        elif isinstance(module, nn.Dropout):
+            R = self.backprop_dropout(R)
+            return R
+        else:
+            raise RuntimeError(f"{type(module)} can not handled currently")
+
+    def backprop_dense(self, activation, module, R):
+        W = torch.clamp(module.weight, min=0)
+        Z = torch.mm(activation, torch.transpose(W, 0, 1)) + 1e-9
+        S = R / Z
+        C = torch.mm(S, W)
+        R = activation * C
+
+        return R
+
+    def backprop_dense_input(self, activation, module, R):
+        W_L = torch.clamp(module.weight, min=0)
+        W_H = torch.clamp(module.weight, max=0)
+
+        L = torch.ones_like(activation, dtype=activation.dtype) * self.lowest
+        H = torch.ones_like(activation, dtype=activation.dtype) * self.highest
+
+        Z_O = torch.mm(activation, torch.transpose(module.weight, 0, 1))
+        Z_L = torch.mm(activation, torch.transpose(W_L, 0, 1))
+        Z_H = torch.mm(activation, torch.transpose(W_H, 0, 1))
+
+        Z = Z_O - Z_L - Z_H + 1e-9
+        S = R / Z
+
+        C_O = torch.mm(S, module.weight)
+        C_L = torch.mm(S, W_L)
+        C_H = torch.mm(S, W_H)
+
+        R = activation * C_O - L * C_L - H * C_H
+
+        return R
+
+    def backprop_conv(self, activation, module, R):
+        stride, padding, kernel = module.stride, module.padding, module.kernel_size
+        output_padding = activation.size(2) - ((R.size(2) - 1) * stride[0] \
+                                        - 2 * padding[0] + kernel[0])
+        W = torch.clamp(module.weight, min=0)
+        Z = F.conv2d(activation, W, stride=stride, padding=padding) + 1e-9
+        S = R / Z
+        C = F.conv_transpose2d(S, W, stride=stride, padding=padding, output_padding=output_padding)
+        R = activation * C
+
+        return R
+
+    def backprop_conv_input(self, activation, module, R):
+        stride, padding, kernel = module.stride, module.padding, module.kernel_size
+        output_padding = activation.size(2) - ((R.size(2) - 1) * stride[0] \
+                                                - 2 * padding[0] + kernel[0])
+
+        W_L = torch.clamp(module.weight, min=0)
+        W_H = torch.clamp(module.weight, max=0)
+
+        L = torch.ones_like(activation, dtype=activation.dtype) * self.lowest
+        H = torch.ones_like(activation, dtype=activation.dtype) * self.highest
+
+        Z_O = F.conv2d(activation, module.weight, stride=stride, padding=padding)
+        Z_L = F.conv2d(L, W_L, stride=stride, padding=padding)
+        Z_H = F.conv2d(H, W_H, stride=stride, padding=padding)
+
+        Z = Z_O - Z_L - Z_H + 1e-9
+        S = R / Z
+
+        C_O = F.conv_transpose2d(S, module.weight, stride=stride, padding=padding, output_padding=output_padding)
+        C_L = F.conv_transpose2d(S, W_L, stride=stride, padding=padding, output_padding=output_padding)
+        C_H = F.conv_transpose2d(S, W_H, stride=stride, padding=padding, output_padding=output_padding)
+
+        R = activation * C_O - L * C_L - H * C_H
+
+        return R
+
+    def backprop_bn(self, R):
+        return R
+
+    def backprop_dropout(self, R):
+        return R
+
+    def backprop_relu(self, activation, R):
+        return R
+
+    def backprop_adap_avg_pool(self, activation, R):
+        kernel_size = activation.shape[-2:]
+        Z = F.avg_pool2d(activation, kernel_size=kernel_size) * kernel_size[0] ** 2 + 1e-9
+        S = R / Z
+        R = activation * S
+
+        return R
+
+    def backprop_max_pool(sef, activation, module, R):
+        kernel_size, stride, padding = module.kernel_size, module.stride, module.padding
+        Z, indices = F.max_pool2d(activation, kernel_size=kernel_size, stride=stride, \
+                                  padding=padding, return_indices=True)
+        Z = Z + 1e-9
+        S = R / Z
+        C = F.max_unpool2d(S, indices, kernel_size=kernel_size, stride=stride, \
+                            padding=padding, output_size=activation.shape)
+        R = activation * C
+
+        return R
+
+    def backprop_divide(self, R0, R1):
+        return R0 + R1
+
+    def backprop_skip_connect(self, activation0, activation1, R):
+        Z = activation0 + activation1 + 1e-9
+        S = R / Z
+        R0 = activation0 * S
+        R1 = activation1 * S
+
+        return (R0, R1)
